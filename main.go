75719ae3a24d1dee163043663d157ab25a0fdf7c
Joel Urbina <joel8080ur@gmail.com>
2024-05-18 17:52:03 -0600

    add security1

--- main.go
+++ main.go
@@ -28,10 +28,10 @@ func main() {
 	app.Use(csrf.New(csrf.Config{
 		KeyLookup:      "header:X-Csrf-Token",
 		CookieName:     "csrf_",
-		CookieSameSite: "Lax",
+		CookieSameSite: "Strict",
 		Expiration:     1 * time.Hour,
 		CookieHTTPOnly: true,
-		CookieSecure:   true,
+		//CookieSecure:   true,
 		KeyGenerator:   utils.UUIDv4,
 		SingleUseToken: true,
 		Extractor: func(c *fiber.Ctx) (string, error) {
@@ -43,7 +43,7 @@ func main() {
 	app.Use(logger.New())
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
+		AllowOrigins:     "http://localhost:5173,https://dark.serveo.net",
 		AllowCredentials: true,
 		AllowMethods:     "GET,POST,PUT,DELETE",
 	}))
