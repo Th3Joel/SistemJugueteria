eeb61c5f9493c750764478ea5a8f156d39c3898c
Joel Urbina <joel8080ur@gmail.com>
2024-05-18 16:26:17 -0600

    add security3

--- middleware/mdd.go
+++ middleware/mdd.go
@@ -18,7 +18,6 @@ func AuthM(c *fiber.Ctx) error {
 	// Obtiene el token de la cabecera de autorización
 	//token := c.Get("key")
 	token := c.Cookies("_key")
-	return c.JSON(c.Cookies("csrf_"))
 
 	//Verificar si el token esta almacenado
 	modelToken := models.Token{}
