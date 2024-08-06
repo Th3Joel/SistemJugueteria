09e267773f0ff03be6e4191eb0b2bbda7afeef83
joel <joel8080ur@gmail.com>
2024-08-05 19:02:59 -0600

    fix executable

--- push.sh
+++ push.sh
@@ -2,9 +2,15 @@
 
 params=$1
 
+#Replace the uri in useFetch.ts
+sed -i '8s/.*/const uri = "\/api"/' web/src/modules/core/hooks/useFetch.ts
+
 cd web
 bun run build
 cd ..
+
+sed -i '8s/.*/const uri = "http:\/\/localhost:5000\/api"/' web/src/modules/core/hooks/useFetch.ts
+
 
 go build -o sis
 
