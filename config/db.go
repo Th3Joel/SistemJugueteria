62e42bd2264d5af86148144003c988db98a26bde
Joel Calderon <joel@localhost.localdomain>
2024-08-12 15:49:47 -0600

    b

--- config/db.go
+++ config/db.go
@@ -22,7 +22,7 @@ var (
 
 func ConnectDB() {
 	dsn := "VwgHAVyMup7XfY6.root:D21sfVrtNmTuZPgy@tcp(gateway01.us-east-1.prod.aws.tidbcloud.com:4000)/jugueteria?parseTime=true&tls=true"
-	//dsn := "joel:joel1234@tcp(127.0.0.1:3306)/jugueteria?charset=utf8mb4&parseTime=true&loc=Local"
+	//dsn := "root:Jo12el34//@tcp(127.0.0.1:3306)/jugueteria?charset=utf8mb4&parseTime=true&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{TranslateError: true, SkipDefaultTransaction: true})
 	if err != nil {
 		log.Fatal("No se pudo conectar a la base de datos. \n")
