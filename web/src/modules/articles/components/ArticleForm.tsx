430538021dd4d38ae3c7dac1698dbe619e41e1de
joel <joel8080ur@gmail.com>
2024-08-20 23:07:05 -0600

    b

--- web/src/modules/articles/components/ArticleForm.tsx
+++ web/src/modules/articles/components/ArticleForm.tsx
@@ -1,5 +1,5 @@
 import { InputText, IOptions } from "@/modules/core/components/InputText.tsx";
-import { FaArrowDownWideShort, FaBarcode, FaBoxOpen, FaDatabase, FaTag } from "react-icons/fa6";
+import { FaArrowDownWideShort, FaBarcode, FaDatabase, FaTag } from "react-icons/fa6";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 import LoaderBtn from "@/modules/core/components/LoaderBtn.tsx";
