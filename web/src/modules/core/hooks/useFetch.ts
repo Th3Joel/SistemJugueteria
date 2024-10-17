78db0c5f292416d62d76c80b3f9849a1b53cdb00
Joel Urbina <joel8080ur@gmail.com>
2024-10-16 22:22:56 -0600

    Building to deploy in serpacloud

--- web/src/modules/core/hooks/useFetch.ts
+++ web/src/modules/core/hooks/useFetch.ts
@@ -5,7 +5,7 @@ export const useFetch = async <T>(
   body?: string | FormData,
   isFormData?: boolean,
 ): Promise<T> => {
-  const uri = "/api"
+const uri = "/api"
 
   const content_type = isFormData ? null : {
     "Content-Type": "application/json",
@@ -19,7 +19,7 @@ export const useFetch = async <T>(
       ...content_type
     },
   });
-
+  
   const res: T = await req.json();
 
 
