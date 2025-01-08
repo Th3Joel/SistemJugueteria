069c5cdf90f8d174976c5c91de26dccbf468171e
joel <joel8080ur@gmail.com>
2025-01-08 10:42:18 -0600

    things

--- config/db.go
+++ config/db.go
@@ -24,7 +24,7 @@ func ConnectDB() {
 	//dsn := "VwgHAVyMup7XfY6.root:D21sfVrtNmTuZPgy@tcp(gateway01.us-east-1.prod.aws.tidbcloud.com:4000)/jugueteria?parseTime=true&tls=true"
 	var db *gorm.DB
 	var err error
-	dsn := "joel:Jo12el34@tcp(db:3306)/jugueteria?charset=utf8mb4&parseTime=true&loc=Local"
+	dsn := "root:Jo12el34//@tcp(localhost:3306)/jugueteria?charset=utf8mb4&parseTime=true&loc=Local"
 	for {
 		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
 			TranslateError:         true,
